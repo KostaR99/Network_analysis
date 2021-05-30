9f109c7b7c0e8fc440c6d47d5f94b943842e54e3
Kosta Rakonjac <42321302+kostarakonjac1331@users.noreply.github.com>
2021-05-30 20:08:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,79 @@
 from implementation.Graph_loading import load_epinions_slash, load_wiki,generate_random_graph, load_small_graph
 from implementation import Clustering
-from implementation.Cluster_analysis import analysis, analyse_graph_of_clusters, analyse_giant_component
+from implementation import Cluster_analysis
 import networkx as nx
-
+import os
+import sys
 '''
 Subject for change
 '''
 
 def load_graph():
-    print("Choose what graph you want to load:")
-    choice = int(input("1) Wiki-RfA\n2)Epinions\n3)Slashdot\n4)Random graph\n5)Small graph\n->"))
     graph = None
-    if choice == 1:
-        graph = load_wiki("D:\graph_clusterability\data\wiki-RfA.txt")
-    elif choice == 2:
-        graph = load_epinions_slash("D:\graph_clusterability\data\epinions.txt")
-    elif choice == 3:
-        graph = load_epinions_slash("D:\graph_clusterability\data\slashdot.txt")
-    elif choice == 4:
-        graph = generate_random_graph(1000,100,clusterable=False)
-    elif choice == 5:
-        graph = load_small_graph()
+    while graph == None:
+        print("Choose what graph you want to load:")
+        choice = input("1) Wiki-RfA\n2)Epinions\n3)Slashdot\n4)Random graph\n5)Small graph\n->")
+        if choice == "1":
+            graph = load_wiki("D:\graph_clusterability\data\wiki-RfA.txt")
+        elif choice == "2":
+            graph = load_epinions_slash("D:\graph_clusterability\data\epinions.txt")
+        elif choice == "3":
+            graph = load_epinions_slash("D:\graph_clusterability\data\slashdot.txt")
+        elif choice == "4":
+            number_of_nodes = int(input("Input number of nodes you want to have in the graph: "))
+            number_of_clusters = int(input("Input number of nodes you want to have in the graph: "))
+            clusterable = input("Do you want this graph to be clusterable (Y/N): ")
+            if clusterable == "y" or clusterable == "Y":
+                clusterable = True
+            else:
+                clusterable = False
+            graph = generate_random_graph(number_of_nodes,number_of_clusters,clusterable=clusterable)
+        elif choice == "5":
+            graph_choice = input("What graph do you want to load:\n1) Clusterable\n2) Unclusterable\n->")
+            if graph_choice == "1":
+                graph = load_small_graph("D:\graph_clusterability\data\small_clusterable_graph.txt")
+            elif graph_choice == "2":
+                graph = load_small_graph("D:\graph_clusterability\data\small_unclusterable_graph.txt")
+        os.system("cls") #for windows, for unix systems it is 'os.system("clear")'
     return graph
+            
 
-def is_graph_clusterable(graph:nx.Graph):
+    
+        
+            
+
+
+def graph_analysis(graph:nx.Graph):
+    print("Results of graph analysis: \n")
     clusters = Clustering.identify_components(graph)
-    print("Number of clusters in the graph is: "+str(len(clusters[0])))
-    cluster_g = Clustering.create_set_of_clusters(clusters[0],clusters[1],graph)
-    coalitions, anticoalitions = Clustering.is_clusterable(cluster_g)
-    print("Number of coalitions: "+str(len(coalitions)))
-    print("Number of anti coalitions: "+str(len(anticoalitions)))
-    analysis(coalitions,anticoalitions)
+    print(f"Number of clusters: {len(clusters)}\n")
+    set_of_clusters = Clustering.create_set_of_clusters(clusters,graph)
+    triplet_set = Clustering.is_clusterable(set_of_clusters)
+    print(f"Number of coalitions: {len(triplet_set[0])}")
+    print(f"Number of anti coalitions: {len(triplet_set[1])}")
+    print(f"Number of links that violate clusterabillity: {len(triplet_set[2])}")
+    if len(triplet_set[2]) != 0:
+        answer = input("Do you want to print edges that need to be removed so the graph becomes clusterable (Y/ any other key): ")
+        if answer == "y" or answer == "Y":
+            print("Edges that need to be removed so the graph becomes clusterable: ")
+            for i in triplet_set[2]:
+                print(i)
+            print("\n")
 
-    giant = Clustering.find_giant_component(cluster_g)
-    print("\nStructure of giant component:\n")
-    analyse_giant_component(graph,giant)
-    graph_of_clusters = Clustering.create_graph_of_clusters(cluster_g,graph)
-    print("\nStructure of graph of clusters:\n")
-    analyse_graph_of_clusters(graph_of_clusters)
+    Cluster_analysis.analysis(triplet_set[0],triplet_set[1])
+    giant_component = Clustering.find_giant_component(set_of_clusters,graph)
+
+    print("\n")
+    Cluster_analysis.analyse_giant_component(graph,giant_component)
+    print("\n")
+    
+    network_of_clusters = Clustering.create_network_of_clusters(set_of_clusters,graph)
+
+    Cluster_analysis.analyse_graph_of_clusters(network_of_clusters)
+
+            
+
+
 if __name__ == '__main__':
     graph = load_graph()
-    is_graph_clusterable(graph)
-
+    graph_analysis(graph)
